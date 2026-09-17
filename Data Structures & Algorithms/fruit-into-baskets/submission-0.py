a76508c4ba6eb7bd6ca1401db4ab602edd5d672f
rajkunal6200 <kunalraj31012005@gmail.com>
2026-09-17 13:41:26 +0530

    Add: fruit-into-baskets - submission-0

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        count ={}
+        left = 0
+        answer =0
+        for right in range(len(fruits)):
+            count[fruits[right]]=count.get(fruits[right],0)+1
+
+            while len(count)>2:
+                count[fruits[left]]-=1
+
+                if count[fruits[left]]==0:
+                    del count[fruits[left]]
+                left+=1
+            answer = max(answer,right-left+1)
+        return answer            
